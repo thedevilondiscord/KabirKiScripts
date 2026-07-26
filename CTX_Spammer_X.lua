02a3f898382460f614059e342194e23dc8ef0701
thedevilondiscord <thedevilondiscord@gmail.com>
2026-07-26 12:25:38 +0530

    CTX_Spammer_X.lua

--- CTX_Spammer_X.lua
+++ CTX_Spammer_X.lua
@@ -1,6 +1,6 @@
 do
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/thedevilondiscord/LuaRepo/refs/heads/main/INTRO"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/thedevilondiscord/LuaRepo/refs/heads/main/publicC_S"))()
 end
 do
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/thedevilondiscord/LuaRepo/refs/heads/main/publicC_S"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/thedevilondiscord/LuaRepo/refs/heads/main/INTRO"))()
 end
